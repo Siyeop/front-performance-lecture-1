da788f23701b4f2a0fcac7fb77e0cd452795e597
Siyeop <siyeop.kim@gmail.com>
2023-10-24 10:19:41 +0900

    3-2. code splitting 적용 - 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import('./pages/ListPage/index'));
+const ViewPage = lazy(() => import('./pages/ViewPage/index'));
+
 
 function App() {
   return (
